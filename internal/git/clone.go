96925b434fd902f04e5e34c366d11775deea65fb
icelanced <maksghg33@gmail.com>
2026-03-14 20:26:03 +0300

    revert: back to simple git clone

--- internal/git/clone.go
+++ internal/git/clone.go
@@ -16,22 +16,13 @@ func Clone(url string) (dir string, cleanup func(), err error) {
 		os.RemoveAll(tempDir)
 	}
 
-	commands := [][]string{
-		{"git", "init", tempDir},
-		{"git", "-C", tempDir, "remote", "add", "origin", url},
-		{"git", "-C", tempDir, "sparse-checkout", "init"},
-		{"git", "-C", tempDir, "sparse-checkout", "set", "go.mod", "go.sum"},
-		{"git", "-C", tempDir, "pull", "--depth=1", "origin", "HEAD"},
-	}
+	cmd := exec.Command("git", "clone", "--depth=1", url, tempDir)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
 
-	for _, args := range commands {
-		cmd := exec.Command(args[0], args[1:]...)
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-		if err := cmd.Run(); err != nil {
-			cleanup()
-			return "", nil, fmt.Errorf("команда %v завершилась с ошибкой: %w", args, err)
-		}
+	if err := cmd.Run(); err != nil {
+		cleanup()
+		return "", nil, fmt.Errorf("git clone завершился с ошибкой: %w", err)
 	}
 
 	return tempDir, cleanup, nil
