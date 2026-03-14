33a028fe3106458808b9a10c1e0b05e841f00023
icelanced <maksghg33@gmail.com>
2026-03-14 15:38:36 +0300

    Optimizing: use sparse-checkout instead of full clone

--- internal/git/clone.go
+++ internal/git/clone.go
@@ -7,9 +7,7 @@ import (
 )
 
 func Clone(url string) (dir string, cleanup func(), err error) {
-
 	tempDir, err := os.MkdirTemp("", "gomod-checker-*")
-
 	if err != nil {
 		return "", nil, fmt.Errorf("не удалось создать temp-директорию: %w", err)
 	}
@@ -18,14 +16,23 @@ func Clone(url string) (dir string, cleanup func(), err error) {
 		os.RemoveAll(tempDir)
 	}
 
-	cmd := exec.Command("git", "clone", "--depth=1", url, tempDir)
-
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-
-	if err := cmd.Run(); err != nil {
-		cleanup()
-		return "", nil, fmt.Errorf("git-clone завершился с ошибкой: %w", err)
+	commands := [][]string{
+		{"git", "init", tempDir},
+		{"git", "-C", tempDir, "remote", "add", "origin", url},
+		{"git", "-C", tempDir, "sparse-checkout", "init"},
+		{"git", "-C", tempDir, "sparse-checkout", "set", "go.mod", "go.sum"},
+		{"git", "-C", tempDir, "pull", "--depth=1", "origin", "HEAD"},
 	}
+
+	for _, args := range commands {
+		cmd := exec.Command(args[0], args[1:]...)
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		if err := cmd.Run(); err != nil {
+			cleanup()
+			return "", nil, fmt.Errorf("команда %v завершилась с ошибкой: %w", args, err)
+		}
+	}
+
 	return tempDir, cleanup, nil
 }
