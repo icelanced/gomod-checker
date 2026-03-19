910dc9673f3c54f5e2cda24b67518baf126217a4
icelanced <maksghg33@gmail.com>
2026-03-19 19:53:44 +0300

    fix: use run() to prevent defer skip on os.Exit

--- cmd/gomod-checker/main.go
+++ cmd/gomod-checker/main.go
@@ -9,10 +9,15 @@ import (
 )
 
 func main() {
-
-	if len(os.Args) < 2 {
-		fmt.Fprintln(os.Stderr, "Использование: gomod-checker <git-repo-url>")
+	if err := run(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
+	}
+}
+
+func run() error {
+	if len(os.Args) < 2 {
+		return fmt.Errorf("использование: gomod-checker <git-repo-url>")
 	}
 
 	repoUrl := os.Args[1]
@@ -20,23 +25,21 @@ func main() {
 	fmt.Printf("Клонирую репозиторий %s\n\n", repoUrl)
 	repoDir, cleanup, err := git.Clone(repoUrl)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Ошибка клонирования: %v\n", err)
-		os.Exit(1)
+		return fmt.Errorf("ошибка клонирования: %w", err)
 	}
 	defer cleanup()
 
 	modInfo, err := gomod.Parse(repoDir)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Ошибка чтения go.mod: %v\n", err)
-		os.Exit(1)
+		return fmt.Errorf("ошибка чтения go.mod: %w", err)
 	}
 
 	fmt.Println("Проверяю наличие обновлений...")
 	updates, err := updater.GetUpdates(repoDir)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Ошибка получения обновлений: %v\n", err)
-		os.Exit(1)
+		return fmt.Errorf("ошибка получения обновлений: %w", err)
 	}
 
 	printResult(modInfo, updates)
+	return nil
 }
